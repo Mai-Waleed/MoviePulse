a0a6fd3c11bb08b705e00a0f7d35ce7888b3f812
Mai-Waleed <maiwaleed452004@gmail.com>
2026-07-19 16:44:03 +0300

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,35 @@
-import { useState } from 'react'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 // import {Offline} from "react-detect-offline"
-import Layout from './components/Layout/Layout'
-import Home from './components/Home/Home'
+import Layout from "./components/Layout/Layout";
+import Home from "./components/Home/Home";
 // import Login from './components/Login/Login'
 // import Register from './components/Register/Register'
-import Movies from './components/Movies/Movies'
-import People from './components/People/People'
-import Tv from './components/Tv/Tv'
-import ItemDetails from './components/ItemDetails/ItemDetails'
-import NotFound from './components/NotFound/NotFound'
+import Movies from "./components/Movies/Movies";
+import People from "./components/People/People";
+import Tv from "./components/Tv/Tv";
+import ItemDetails from "./components/ItemDetails/ItemDetails";
+import NotFound from "./components/NotFound/NotFound";
 
-// let routers = createHashRouter([
-//   {
-//     path: "/",
-//     element: <Layout />,
-//     children: [
-//       { index: true, element: <Home /> },
-//       { path: "movies", element: <Movies /> },
-//       { path: "people", element: <People /> },
-//       { path: "tv", element: <Tv /> },
-//       { path: "itemdetails/:media_type/:id", element: <ItemDetails /> },
-//       { path: "*", element: <NotFound /> },
-//     ],
-//   },
-// ]);
+let routers = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "movies", element: <Movies /> },
+      { path: "people", element: <People /> },
+      { path: "tv", element: <Tv /> },
+      { path: "itemdetails/:media_type/:id", element: <ItemDetails /> },
+      { path: "*", element: <NotFound /> },
+    ],
+  },
+]);
 
 function App() {
- return (
-   <>
-     <BrowserRouter basename="/">
-     <Route>
-        <Route path="/" element={<Home />} />
-        <Route path="/" element={<Layout />}>
-          <Route path="movies" element={<Movies />} />
-          <Route path="tv" element={<Tv />} />
-          <Route path="people" element={<People />} />
-          <Route path="itemdetails/:media_type/:id" element={<ItemDetails />} />
-          <Route path="*" element={<NotFound />} />
-          </Route>
-      </Route>
-     </BrowserRouter>
-     {/* <Offline>
+  return (
+    <>
+      {/* <Offline>
        <div className="offline">
          <div className="offline-icon">
            <i className="fas fa-wifi"></i>
@@ -53,9 +41,9 @@ function App() {
          </div>
        </div>
      </Offline> */}
-     {/* <RouterProvider router={routers}></RouterProvider> */}
-   </>
- );
+      <RouterProvider router={routers}></RouterProvider>
+    </>
+  );
 }
 
-export default App
+export default App;
