51648afa00cdc13f9d6d5960d092d13eb4e3def9
Mai-Waleed <maiwaleed452004@gmail.com>
2026-07-19 15:52:17 +0300

    fix: update routing to use BrowserRouter and simplify route definitions

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import {createHashRouter, RouterProvider} from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 // import {Offline} from "react-detect-offline"
 import Layout from './components/Layout/Layout'
 import Home from './components/Home/Home'
@@ -11,24 +11,36 @@ import Tv from './components/Tv/Tv'
 import ItemDetails from './components/ItemDetails/ItemDetails'
 import NotFound from './components/NotFound/NotFound'
 
-let routers = createHashRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    children: [
-      { index: true, element: <Home /> },
-      { path: "movies", element: <Movies /> },
-      { path: "people", element: <People /> },
-      { path: "tv", element: <Tv /> },
-      { path: "itemdetails/:media_type/:id", element: <ItemDetails /> },
-      { path: "*", element: <NotFound /> },
-    ],
-  },
-]);
+// let routers = createHashRouter([
+//   {
+//     path: "/",
+//     element: <Layout />,
+//     children: [
+//       { index: true, element: <Home /> },
+//       { path: "movies", element: <Movies /> },
+//       { path: "people", element: <People /> },
+//       { path: "tv", element: <Tv /> },
+//       { path: "itemdetails/:media_type/:id", element: <ItemDetails /> },
+//       { path: "*", element: <NotFound /> },
+//     ],
+//   },
+// ]);
 
 function App() {
  return (
    <>
+     <BrowserRouter basename="/">
+     <Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Layout />}>
+          <Route path="movies" element={<Movies />} />
+          <Route path="tv" element={<Tv />} />
+          <Route path="people" element={<People />} />
+          <Route path="itemdetails/:media_type/:id" element={<ItemDetails />} />
+          <Route path="*" element={<NotFound />} />
+          </Route>
+      </Route>
+     </BrowserRouter>
      {/* <Offline>
        <div className="offline">
          <div className="offline-icon">
@@ -41,7 +53,7 @@ function App() {
          </div>
        </div>
      </Offline> */}
-     <RouterProvider router={routers}></RouterProvider>
+     {/* <RouterProvider router={routers}></RouterProvider> */}
    </>
  );
 }
